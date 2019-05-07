7af3e28d3383412bcb4143fe4f029be9b5bfecc8
Pierre <pierre.de.vreyer@gmail.com>
2019-05-07 13:52:24 +0800

    Started the configuration of ActionCable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :require_user
 
   def create
-    message = current_user.build(message_params)
+    message = current_user.messages.build(message_params)
     if message.save
       redirect_to root_path
     end
