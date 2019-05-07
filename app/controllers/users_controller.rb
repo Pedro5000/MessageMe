b540fb315ea9a0bac32b2adc23470251138266bb
Pierre <pierre.de.vreyer@gmail.com>
2019-05-07 18:06:28 +0800

    Added the sign up functionality to the app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to Message Me, #{@user.username}!"
+      redirect_to root_path
+    else
+      flash[:error] = "There was something wrong with your sign up information."
+      redirect_to signup_path
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
